19ecef46775f4c786d273a868a05c2fd1c7a459e
Sasemohan <sasemohan90950@gmail.com>
2016-07-13 22:29:26 +0530

    Update rect.c

--- rect.c
+++ rect.c
@@ -7,14 +7,17 @@ int main(){
 int side1,side2,side3,side4,side5,side6,side7,side8;
    int gd = DETECT,gm;
    initgraph(&gd, &gm, "C:\\TC\\BGI");
-scanf("%d%d%d%d%d%d%d%d",
+   printf("enter the value for the first rectangle");
+scanf("%d%d%d%d",&side1,&side2,&side3,&side4);
+prinntf("enter the value for the second rectangle");
+scanf("%d%d%d%d",&side5,&side6,&side7,&side8);
    /* Draw rectangle on screen */
    rectangle(side1, side2,side3, side4);
    rectangle(side5,side6,side7,side8);
-   if(side1<=side5 && side3<=side7)
+   if(side1<=side5 || side5<=side1) &&(side3<=side6 || side6<=side3)
    {
    printf("true");
-
+}
   
 
    getch();
